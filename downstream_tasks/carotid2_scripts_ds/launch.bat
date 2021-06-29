f89fc9cc08d94b0645cec2e2da233051347ce27b
chingheng113 <chingheng113@gmail.com>
2021-06-29 22:09:43 +0800

    bk

---
+++ downstream_tasks/carotid2_scripts_ds/launch.bat
@@ -0,0 +1,11 @@
+#!/bin/sh
+swarm -f carotid2_sb_ds_0.swarm -g 100 -t 6 --job-name carotid2_sb_all_0 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_1.swarm -g 100 -t 6 --job-name carotid2_sb_all_1 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_2.swarm -g 100 -t 6 --job-name carotid2_sb_all_2 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_3.swarm -g 100 -t 6 --job-name carotid2_sb_all_3 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_4.swarm -g 100 -t 6 --job-name carotid2_sb_all_4 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_5.swarm -g 100 -t 6 --job-name carotid2_sb_all_5 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_6.swarm -g 100 -t 6 --job-name carotid2_sb_all_6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_7.swarm -g 100 -t 6 --job-name carotid2_sb_all_7 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_8.swarm -g 100 -t 6 --job-name carotid2_sb_all_8 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f carotid2_sb_ds_9.swarm -g 100 -t 6 --job-name carotid2_sb_all_9 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
