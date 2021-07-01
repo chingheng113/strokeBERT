51e9feead55292ed99f23527ddae606f2cf6537e
chingheng113 <chingheng113@gmail.com>
2021-07-01 11:23:01 +0800

    bk

--- downstream_tasks/restroke_scripts/all_days/launch.bat
+++ downstream_tasks/restroke_scripts/all_days/launch.bat
@@ -1,14 +1,26 @@
 #!/bin/sh
-swarm -f restroke_sb_ds_0.swarm -g 100 -t 6 --job-name restroke_sb_ds_0 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_1.swarm -g 100 -t 6 --job-name restroke_sb_ds_1 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_2.swarm -g 100 -t 6 --job-name restroke_sb_ds_2 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_3.swarm -g 100 -t 6 --job-name restroke_sb_ds_3 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_4.swarm -g 100 -t 6 --job-name restroke_sb_ds_4 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_5.swarm -g 100 -t 6 --job-name restroke_sb_ds_5 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_6.swarm -g 100 -t 6 --job-name restroke_sb_ds_6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_7.swarm -g 100 -t 6 --job-name restroke_sb_ds_7 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_8.swarm -g 100 -t 6 --job-name restroke_sb_ds_8 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
-swarm -f restroke_sb_ds_9.swarm -g 100 -t 6 --job-name restroke_sb_ds_9 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_0.swarm -g 100 -t 6 --job-name restroke_sb_nu_0 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_1.swarm -g 100 -t 6 --job-name restroke_sb_nu_1 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_2.swarm -g 100 -t 6 --job-name restroke_sb_nu_2 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_3.swarm -g 100 -t 6 --job-name restroke_sb_nu_3 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_4.swarm -g 100 -t 6 --job-name restroke_sb_nu_4 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_5.swarm -g 100 -t 6 --job-name restroke_sb_nu_5 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_6.swarm -g 100 -t 6 --job-name restroke_sb_nu_6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_7.swarm -g 100 -t 6 --job-name restroke_sb_nu_7 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_8.swarm -g 100 -t 6 --job-name restroke_sb_nu_8 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+swarm -f restroke_sb_nu_9.swarm -g 100 -t 6 --job-name restroke_sb_nu_9 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+
+
+#swarm -f restroke_sb_ds_0.swarm -g 100 -t 6 --job-name restroke_sb_ds_0 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_1.swarm -g 100 -t 6 --job-name restroke_sb_ds_1 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_2.swarm -g 100 -t 6 --job-name restroke_sb_ds_2 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_3.swarm -g 100 -t 6 --job-name restroke_sb_ds_3 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_4.swarm -g 100 -t 6 --job-name restroke_sb_ds_4 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_5.swarm -g 100 -t 6 --job-name restroke_sb_ds_5 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_6.swarm -g 100 -t 6 --job-name restroke_sb_ds_6 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_7.swarm -g 100 -t 6 --job-name restroke_sb_ds_7 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_8.swarm -g 100 -t 6 --job-name restroke_sb_ds_8 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
+#swarm -f restroke_sb_ds_9.swarm -g 100 -t 6 --job-name restroke_sb_ds_9 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
 
 #swarm -f restroke_sb_all_0.swarm -g 100 -t 6 --job-name restroke_sb_all_0 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
 #swarm -f restroke_sb_all_1.swarm -g 100 -t 6 --job-name restroke_sb_all_1 --module python/3.6 --gres=gpu:p100:2 --partition=gpu
