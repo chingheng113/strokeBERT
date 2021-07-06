0ebcaecead2a85c9bcc7a9926719f5d06e17dd33
Ching-Heng Lin <chingheng113@gmail.com>
2021-07-06 14:46:17 +0800

    bk

--- downstream_tasks/output/performance_carotid_single.py
+++ downstream_tasks/output/performance_carotid_single.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report
 import os
 import pickle
+import numpy as np
 
-model_name = 's_all_0'
+model_name = 'sb_24'
 print(model_name)
 read_path = os.path.join('carotid', model_name, 'test_prediction.pickle')
 labels = ['RCCA', 'REICA', 'RIICA', 'RACA', 'RMCA', 'RPCA', 'REVA', 'RIVA', 'BA', 'LCCA', 'LEICA', 'LIICA',
@@ -12,20 +13,15 @@ with open(read_path, 'rb') as f:
     data = pickle.load(f)
     all_logits = data['all_logits']
     all_labels = data['all_labels']
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
+    fprs = []
+    tprs = []
+    roc_aucs = []
     for i in range(17):
-        fpr[i], tpr[i], _ = roc_curve(all_labels[:, i], all_logits[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
+        fpr, tpr, _ = roc_curve(all_labels[:, i], all_logits[:, i])
+        roc_aucs.append(auc(fpr, tpr))
         print(labels[i])
-        print(roc_auc[i])
+        print(auc(fpr, tpr))
         p_label_b = (all_logits[:, i] > 0.5).astype(int)
-        print(classification_report(all_labels[:, i], p_label_b))
-        print(confusion_matrix(all_labels[:, i], p_label_b))
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(all_labels.ravel(), all_logits.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-    print('------')
-    print(roc_auc["micro"])
+
+    print(round(np.mean(roc_aucs),3), round(np.std(roc_aucs),3))
     print('done')
